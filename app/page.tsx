8aa64fa3b6145ff356930356e383b68a58a37ff6
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-01-20 17:24:52 +0500

    Section name update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import About from '@/sections/About'
 import Features from '@/sections/Features'
 import Pricing from '@/sections/Pricing'
 import FAQ from '@/sections/FAQ'
-import Newsletter from '@/sections/Newsletter'
+import CTA from '@/sections/CTA'
 import Footer from '@/sections/Footer'
 
 export default function Home() {
@@ -16,7 +16,7 @@ export default function Home() {
       <Features />
       <Pricing />
       <FAQ />
-      <Newsletter />
+      <CTA />
       <Footer />
     </>
   )
