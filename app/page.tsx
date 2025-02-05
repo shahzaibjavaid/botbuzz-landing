758da6c5ba69ae07eadc62d1130cb70620ae285d
Shahzaib Javaid <shahzaibjavaid68@gmail.com>
2025-02-05 17:52:07 +0500

    add loading screen animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from 'react'
 import Header from '@/sections/Header'
 import Hero from '@/sections/Hero'
 import About from '@/sections/About'
@@ -7,17 +9,27 @@ import FAQ from '@/sections/FAQ'
 import CTA from '@/sections/CTA'
 import Footer from '@/sections/Footer'
 
+import { LoadingScreen } from '@/components/LoadingScreen'
+
 export default function Home() {
+  const [isLoaded, setIsLoaded] = useState(false)
   return (
     <>
-      <Header />
-      <Hero />
-      <About />
-      <Pricing />
-      <Features />
-      <FAQ />
-      <CTA />
-      <Footer />
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{' '}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        } bg-black text-gray-100`}
+      >
+        <Header />
+        <Hero />
+        <About />
+        <Pricing />
+        <Features />
+        <FAQ />
+        <CTA />
+        <Footer />
+      </div>
     </>
   )
 }
